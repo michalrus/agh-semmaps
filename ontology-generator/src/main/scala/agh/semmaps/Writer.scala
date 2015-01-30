ea7295b00ecbce8d6deb4dcb47c067856240f59d
Michal Rus <m@michalrus.com>
2015-01-30 21:31:28 +0100

    Save the ontology to a file, actually

--- ontology-generator/src/main/scala/agh/semmaps/Writer.scala
+++ ontology-generator/src/main/scala/agh/semmaps/Writer.scala
@@ -1,6 +1,6 @@
 package agh.semmaps
 
-import java.io.File
+import java.io.{ PrintWriter, File }
 
 import scala.util.Try
 
@@ -10,18 +10,25 @@ trait Writer {
 
 object PrologWriter extends Writer {
   def apply(output: File)(ontology: Ontology): Try[Unit] = Try {
-    TreeBuilder.prettyPrint(ontology.trees)
-
     val DumpResult(dmp, classesUsed) = (ontology.trees foldLeft DumpResult("", Set.empty)) {
       case (acc, tree) ⇒
         dump(0)(tree) match {
-          case Some(dr) ⇒ DumpResult(acc.dump + ".\n" + dr.dump, acc.classesUsed ++ dr.classesUsed)
+          case Some(dr) ⇒ DumpResult(acc.dump + (if (acc.dump.nonEmpty) ".\n" else "") + dr.dump, acc.classesUsed ++ dr.classesUsed)
           case None     ⇒ acc
         }
     }
-    classesUsed foreach (cl ⇒ println(s"class($cl)."))
-    println()
-    println(dmp + ".")
+    val result =
+      s"""relation(has).
+         |relation(consists_of).
+         |
+         |${classesUsed map (cl ⇒ s"class($cl).") mkString "\n"}
+         |
+         |$dmp.""".stripMargin
+    if (output.toString == "-") println(result) else {
+      val pw = new PrintWriter(output)
+      pw.write(result)
+      pw.close()
+    }
   }
   final case class DumpResult(dump: String, classesUsed: Set[String])
   def dump(indent: Int)(tree: JmlTree): Option[DumpResult] = {
@@ -34,11 +41,8 @@ object PrologWriter extends Writer {
     def props(m: Map[String, String]): String = m map { case (k, v) ⇒ s"""${k.toLowerCase}: "$v"""" } mkString ", "
 
     tree.node.tpe match {
-      case JmlArea ⇒
-        Some(DumpResult(i + "classroom{}" + hasText, Set("classroom") ++ chClass))
-
-      case JmlObstacle | JmlPoi ⇒
-        val kind = tree.node.props.getOrElse("Kind", "unnamed obstacle").replace(" ", "_").toLowerCase
+      case JmlArea | JmlObstacle | JmlPoi ⇒
+        val kind = tree.node.props.getOrElse("Kind", "unnamed").replace(" ", "_").toLowerCase
         Some(DumpResult(i + s"$kind{${props(tree.node.props filterKeys (_ != "Kind"))}}" + hasText, Set(kind) ++ chClass))
 
       case JmlDoor ⇒
