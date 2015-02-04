1d35e3e757a23e9eeed2f4a5bb1e400022ccdc5f
Michal Rus <m@michalrus.com>
2015-02-04 09:55:11 +0100

    Split `features` into separate `<feature>:true`

--- ontology-generator/src/main/scala/agh/semmaps/Writer.scala
+++ ontology-generator/src/main/scala/agh/semmaps/Writer.scala
@@ -34,22 +34,29 @@ object PrologWriter extends Writer {
       pw.close()
     }
   }
+  val Prologize = """[^a-z0-9_]""".r
+  def prologize(id: String) = Prologize.replaceAllIn(id.toLowerCase, "_")
+
   final case class DumpResult(dump: String, classesUsed: Set[String])
   def dump(indent: Int)(tree: JmlTree): Option[DumpResult] = {
     val i = "  " * indent
 
     val children = (tree.children.toList map dump(indent + 1)).flatten
 
-    def redundant(xs: List[String]): List[String] = {
-      (xs groupBy identity mapValues (_.size) map { case (x, num) ⇒ x + (if (num > 1) s" * $num" else "") }).toList
-    }
-    val hasText = if (children.isEmpty) " exists" else " has [\n" + redundant(children.map(_.dump).sorted).mkString(",\n") + "\n" + i + "]"
+    val hasText = if (children.isEmpty) " exists" else " has [\n" + children.map(_.dump).mkString(",\n") + "\n" + i + "]"
     val chClass = children.flatMap(_.classesUsed)
-    def props(node: JmlObject): String = ((node.props filterKeys (_ != "Kind")) +
-      ("uuid" → tree.node.uuid.toString)) map { case (k, v) ⇒ s"""${k.toLowerCase}: "$v"""" } mkString ", "
+
+    def props(node: JmlObject): String = {
+      val ps = ((node.props filterKeys (_ != "Kind")) +
+        ("uuid" → tree.node.uuid.toString)) flatMap {
+          case (k, v) if k.toUpperCase == "FEATURES" ⇒ v.split(';') map (_.trim) filter (_.nonEmpty) map (prologize(_) → "true")
+          case x                                     ⇒ Map(x)
+        }
+      ps map { case (k, v) ⇒ s"""${k.toLowerCase}: "$v"""" } mkString ", "
+    }
 
     val kind = tree.node.tpe match {
-      case JmlArea | JmlObstacle | JmlPoi ⇒ tree.node.props.getOrElse("Kind", "unnamed").replace(" ", "_").toLowerCase
+      case JmlArea | JmlObstacle | JmlPoi ⇒ prologize(tree.node.props.getOrElse("Kind", "unnamed"))
       case JmlDoor                        ⇒ "door"
     }
 
