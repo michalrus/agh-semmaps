67fff2e3ebf40f108f865aaa68fc003e3fdca175
Michal Rus <m@michalrus.com>
2015-05-11 19:59:07 +0200

    Clean up the mind map even more

--- question-generator/src/main/scala/agh/semmaps/QuestionGenerator.scala
+++ question-generator/src/main/scala/agh/semmaps/QuestionGenerator.scala
@@ -10,10 +10,20 @@ object QuestionGenerator {
   def apply(alternatives: Set[JmlTree]) = {
     // 1. Build the Set from trees of alternatives
 
-    // 2. Choose a question param with the lowest entropy
+    // 2. Choose a question param with the lowest entropy and cost combined
+
+    // 3. Ask the question
+
+    // 4. Get rid of non-matching alternatives
+
+    // 5. Loop
 
   }
 
   def buildSet(alternative: JmlTree): Set[QuestionParam] = ???
+
+  trait ParamSetBuilder {
+    def apply(alternative: JmlTree): Set[(QuestionParam, Answer)]
+  }
 
 }
