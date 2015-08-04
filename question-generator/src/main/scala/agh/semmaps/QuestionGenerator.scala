024efa898f15413db4d1258a700a2e54dc2a6067
Michal Rus <m@michalrus.com>
2015-08-04 17:30:12 +0200

    Create a map of costs for each JmlObject

--- question-generator/src/main/scala/agh/semmaps/QuestionGenerator.scala
+++ question-generator/src/main/scala/agh/semmaps/QuestionGenerator.scala
@@ -29,7 +29,7 @@ object QuestionGenerator {
   // final case class Relation(...) extends Question[???] // TODO
 
   /** Question generator for given alternatives and costs map */
-  type QuestionSetGen = (Set[JmlTree], Map[JmlObject, Double]) ⇒ Set[Question[_]]
+  type QuestionSetGen = (Set[JmlTree], Map[JmlObject, Map[Option[String], Double]]) ⇒ Set[Question[_]]
 
   /** Primitively flattens JML trees and compares only classes (in ontological sense) of objects that exists in alternative rooms */
   val SimpleClassExists: QuestionSetGen = { (alternatives, costs) ⇒
@@ -37,12 +37,12 @@ object QuestionGenerator {
       (flattened(alternative) map (obj ⇒ (obj.className, costs(obj)) → alternative)).toSeq
     ).fold(Seq.empty)(_ ++ _).groupBy(_._1).mapValues(_.map(_._2).toSet).map {
       case ((className, cost), alts) ⇒
-        Exists(className, None, Map(true → alts, false → (alternatives -- alts)), cost): Question[_]
+        Exists(className, None, Map(true → alts, false → (alternatives -- alts)), cost.getOrElse(None, 0.0)): Question[_]
     }.toSet
   }
 
   def apply(alternatives: Set[JmlTree], costRules: Set[Cost]): Unit = {
-    // 0. Build the hash map of costs (point of possible optimization)
+    // 0. Build the map of costs (point of possible optimization)
     val costs = (alternatives map costsMap(costRules)).fold(Map.empty)(_ ++ _)
 
     // 1. Build the question Set from trees of alternatives
@@ -62,8 +62,22 @@ object QuestionGenerator {
   def flattened(tree: JmlTree): Set[JmlObject] =
     (tree.children flatMap flattened) + tree.node
 
-  def costsMap(costRules: Set[Cost])(tree: JmlTree): Map[JmlObject, Double] = {
-    ??? // TODO
+  /** Assigns property→cost map to each JmlObject; property==None means “just mentioning the object” */
+  def costsMap(costRules: Set[Cost])(tree: JmlTree): Map[JmlObject, Map[Option[String], Double]] = {
+    def isSelectorMatching(o: JmlObject, sel: Map[String, String]): Boolean =
+      sel forall { case (k, v) ⇒ o.props.get(k) contains v }
+
+    def isMultiSelectorMatching(path: List[JmlObject])(cost: Cost): Boolean =
+      (path.reverse zip cost.objectSelector.reverse) forall { case (o, sel) ⇒ isSelectorMatching(o, sel) }
+
+    def loopOverPaths(parents: List[JmlObject])(startAt: JmlTree): Seq[(JmlObject, Option[String], Double)] = {
+      val me = startAt.node
+      val meWithParents = parents :+ me
+      val myCosts = (costRules filter isMultiSelectorMatching(meWithParents)).toSeq map (c ⇒ (me, c.property, c.change.toDouble))
+      (startAt.children.toSeq flatMap loopOverPaths(meWithParents)) ++ myCosts
+    }
+
+    loopOverPaths(Nil)(tree).groupBy(_._1).mapValues(_.groupBy(_._2).mapValues(_.map(_._3).sum))
   }
 
 }
